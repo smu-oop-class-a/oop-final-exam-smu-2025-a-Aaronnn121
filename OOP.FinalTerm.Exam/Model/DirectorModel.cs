fbdaa40fcbac1c476553d981862e1794dd7f173e
MarkAaronEstanoco <MarkAaronEstanoco@SMU-0032>
2025-12-11 18:18:48 +0800

    DirectorModel inherit from PersonModel

--- OOP.FinalTerm.Exam/Model/DirectorModel.cs
+++ OOP.FinalTerm.Exam/Model/DirectorModel.cs
@@ -1,10 +1,15 @@
 ﻿namespace OOP.FinalTerm.Exam.Model
 {
-    //TODO: Students will implement the DirectorModel class with properties as per requirements
+    
     //Inherit PersonModel
     //PersonModel must be created first if not already present
-    public class DirectorModel
+    public class DirectorModel : PersonModel
     {
+        
+        public int TotalMoviesCreated { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Genres { get; set; }
         //add properties here
         //refer to README.md for property details
         //refer to MovieModel for guidance
